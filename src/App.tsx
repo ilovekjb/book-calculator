c91ce1ad03ca830814e879955c2455c1e9877dc5
KHeo <sainthkh@gmail.com>
2021-08-31 10:32:10 +0900

    styles, search input, result.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import { useState } from 'react'
-import './App.css';
+import './style.css'
 import { books as bookList, Book } from './books'
 
 function App() {
   const [books, setState] = useState<Book[]>(bookList)
+  const [search, setSearch] = useState('')
 
   return (
-    <div className="">
-      <header className="">
+    <div className="content">
+      <header className="title">
         <h1>도서출판 킹제임스 도서 목록</h1>
       </header>
+      <div style={{marginBottom: 16}}>
+        <input 
+          type="text"
+          className="search"
+          placeholder="찾고 싶은 책을 입력하세요" 
+          onChange={(e) => {
+            setSearch(e.target.value)
+          }}
+        />
+      </div>
       <div>
-        <table>
+        <table style={{ width: '100%'}}>
           <thead>
-            <th>
-              <td><input type="checkbox" /></td>
-              <td>제목</td>
-              <td>가격</td>
-              <td>수량</td>
-            </th>
+            <tr>
+              <th></th>
+              <th>제목</th>
+              <th className="price">가격</th>
+              <th className="number">수량</th>
+            </tr>
           </thead>
           <tbody>
-            { books.map((book, i) => {
+            { (search === '' ? books : books.filter((book) => book.title.includes(search))).map((book, i) => {
               return (
                 <tr>
                   <td>
@@ -48,9 +59,9 @@ function App() {
                     }} />
                   </td>
                   <td>{book.title}</td>
-                  <td>{book.discountedPrice}</td>
-                  <td>
-                    <input type="number" min={0} value={book.count} onChange={(e) => { 
+                  <td className="price">{book.discountedPrice}</td>
+                  <td className="number">
+                    <input className="number-input" type="number" min={0} value={book.count} onChange={(e) => { 
                       const newBooks = books.map((item, index) => {
                         if (index === i) {
                           const count = parseInt(e.target.value)
@@ -73,9 +84,29 @@ function App() {
       </div>
       <div>
         <h2>주문할 책들</h2>
-        {books.filter(book => book.count > 0).map((item) => {
-          return <p>{item.title}</p>
-        })}
+        <table style={{ width: '100%'}}>
+          <thead>
+            <tr>
+              <th>제목</th>
+              <th className="number">수량</th>
+              <th className="price">가격</th>
+            </tr>
+          </thead>
+          <tbody>
+          {books.filter(book => book.count > 0).map((item) => {
+            return (
+              <tr>
+                <td>{item.title}</td>
+                <td className="number">{item.count}</td>
+                <td className="price">{item.discountedPrice * item.count}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+        <p>총액: {books.reduce((sum, book) => {
+          return sum + book.discountedPrice * book.count
+        }, 0)}</p>
       </div>
     </div>
   );
