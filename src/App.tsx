d9636f527bef526617f70b0b0790ce568564bcb2
KHeo <sainthkh@gmail.com>
2021-09-09 12:15:23 +0900

    shipping fee.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,39 @@ import { paid, free, Book } from './books'
 function App() {
   const [books, setState] = useState<Book[]>([...paid, ...free])
   const [search, setSearch] = useState('')
+  const [showMsg, setShowMsg] = useState(false)
+
+  const booksTotal = books.reduce((sum, book) => {
+    return sum + book.discountedPrice * book.count
+  }, 0)
+  const booksCount = books.reduce((sum, book) => {
+    return sum + book.count
+  }, 0)
+  const buying = books.filter(book => book.count > 0)
+  const buyingTitles = buying.map(book => book.title)
+
+  const hasPaid = paid.filter(book => buyingTitles.includes(book.title)).length > 0
+  let shipping = 0
+
+  if (hasPaid) {
+    if (booksTotal >= 60000) {
+      shipping = 0
+    } else if (booksCount <= 5) {
+      shipping = 3000
+    } else if (booksCount <= 10) {
+      shipping = 4000
+    } else {
+      shipping = 5000
+    }
+  } else {
+    if (booksTotal >= 20000) {
+      shipping = 0
+    } else {
+      shipping = 3000
+    }
+  }
+
+  const total = booksTotal + shipping
 
   return (
     <div className="content">
@@ -66,8 +99,6 @@ function App() {
                           return item
                         })
 
-                        const buyingTitles = newBooks.filter(book => book.checked).map(book => book.title)
-
                         if (paid.filter(book => buyingTitles.includes(book.title)).length === 0) {
                           newBooks = newBooks.map(item => {
                             if (item.discountedPrice === 0) {
@@ -108,8 +139,6 @@ function App() {
                           })
                         }
                       } else {
-                        const buyingTitles = newBooks.filter(book => book.checked).map(book => book.title)
-
                         if (paid.filter(book => buyingTitles.includes(book.title)).length === 0) {
                           newBooks = newBooks.map(item => {
                             if (item.discountedPrice === 0) {
@@ -122,7 +151,7 @@ function App() {
                       }
 
                       setState(newBooks)
-                    }}/>
+                    }}/>권
                   </td>
                 </tr>
               )
@@ -131,37 +160,64 @@ function App() {
         </table>
       </div>
       <div>
-        <h2>주문할 책들</h2>
-        <div>
-          <table style={{ width: '100%'}}>
-            <thead>
-              <tr>
-                <th>제목</th>
-                <th className="number">수량</th>
-                <th className="price">가격</th>
-              </tr>
-            </thead>
-            <tbody>
-            {books.filter(book => book.count > 0).map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.title}</td>
-                  <td className="number">{item.count}권</td>
-                  <td className="price">{item.discountedPrice * item.count}원</td>
+        <h2 className="title">주문할 책들</h2>
+        {
+          buying.length > 0 ? (
+            <div>
+              <table style={{ width: '100%'}}>
+                <thead>
+                  <tr>
+                    <th>제목</th>
+                    <th className="number">수량</th>
+                    <th className="price">가격</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {buying.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.title}</td>
+                      <td className="number">{item.count}권</td>
+                      <td className="price">{item.discountedPrice * item.count}원</td>
+                    </tr>
+                  )
+                })}
+                <tr>
+                  <td>배송료</td>
+                  <td></td>
+                  <td className="price">{shipping}원</td>
                 </tr>
-              )
-            })}
-            </tbody>
-          </table>
-          <p>총액: {books.reduce((sum, book) => {
-            return sum + book.discountedPrice * book.count
-          }, 0)}</p>
-        </div>
-        <div className="button-wrap">
-          <button className="copy-btn" onClick={() => {
-            navigator.clipboard.writeText('text to copy');
-          }}>복사하기</button>
-        </div>
+                <tr style={{borderTop: '1px solid #999'}}>
+                  <td>총액</td>
+                  <td></td>
+                  <td className="price">{total}원</td>
+                </tr>
+                </tbody>
+              </table>
+              <div className="button-wrap">
+                <button className="copy-btn" onClick={() => {
+                  const text = buying.map(book => `${book.title} ${book.count}권`).join(', ')
+
+                  navigator.clipboard.writeText(text);
+                  
+                  setShowMsg(true)
+
+                  setTimeout(() => {
+                    setShowMsg(false)
+                  }, 5000)
+                }}>복사하기</button>
+              </div>
+              {
+                showMsg 
+                ? <div className="msg">복사되었습니다. 주문글에 "책이름"에 붙여 넣으세요.</div>
+                : null
+              }
+            </div>
+          ) : (
+            <div className="msg">주문할 책이 없습니다. 주문하고 싶은 책을 위에서 선택하세요.</div>
+          )
+        }
+
       </div>
     </div>
   );
